374e2cd1b501d76eb3b6b2b1372ab00692a353ba
Techxicn_ <jjangel2815@gmail.com>
2025-02-04 08:55:40 -0600

    Asignacion de listas

--- PFBiblioteca.java
+++ PFBiblioteca.java
@@ -4,9 +4,31 @@ public class PFBiblioteca{
     public static void main(String[] args){
 
         //Variables
-        String newRegist;               // registo de nuevos libros & usuarios | variable acumulada en una lista
-        ArrayList<String> booksOnStock; // lista de almacenamiento de libros en existencia | sub listas en base a géneros/autores
-        
+        String newUserRegist;               // registo de nuevos usuarios | variable acumulada en una lista
+        String newBookRegist;                // registro de un nuevo ingreso de un libro 
+        // Listas de géneros 
+        ArrayList<String>Terror = new ArrayList<>(); 
+        ArrayList<String>Novelas = new ArrayList<>();
+        ArrayList<String>Ficcion = new ArrayList<>();
+        ArrayList<String>Aventuras = new ArrayList<>();
+        ArrayList<String>Clasicos = new ArrayList<>();
+        ArrayList<String>Drama = new ArrayList<>();
+        ArrayList<String>Mitologia = new ArrayList<>();
+        ArrayList<String>Cuentos = new ArrayList<>();
+        ArrayList<String>Romance = new ArrayList<>();
+        //  Listas
+        ArrayList<ArrayList<String>> booksOnStock = new ArrayList<>(); // lista de almacenamiento de libros en existencia | sub listas en base a géneros/autores
+        booksOnStock.add(Terror);
+        booksOnStock.add(Novelas);
+        booksOnStock.add(Ficcion);
+        booksOnStock.add(Aventuras);
+        booksOnStock.add(Clasicos);
+        booksOnStock.add(Drama);
+        booksOnStock.add(Mitologia);
+        booksOnStock.add(Cuentos);
+        booksOnStock.add(Romance);
+
+        ArrayList<String> Users = new ArrayList<>();
         // Interfaz de usuario
         /*
          *  Añadir funciones si hacen falta, de forma progresiva
