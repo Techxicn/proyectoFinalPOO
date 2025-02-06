d4842dcc26931f41fb5f03bd52da63ff679d5a19
johnecoa <john.ecoa@gmail.com>
2025-02-06 11:36:24 -0600

    se añadio la función imprimirLibro y buscarLibro

--- PFBiblioteca.java
+++ PFBiblioteca.java
@@ -3,6 +3,13 @@ import java.util.ArrayList;
 // <THIS>  hace alusion a si mismo, 
 public class PFBiblioteca{
 
+    public static void printMenu(Scanner scanner){
+        System.out.println("Bienvenido a la biblioteca virtual en Java");
+        System.out.println("¿Qué tarea desea realizar?");
+        System.out.println();
+        System.out.println("\n 1.- Buscar libro. \n 2.- Solicitar libros (max. 2).\n 3.- Registrar nuevo usuario.\n 4.- Registrar/devolver libro.\n 5.-  ");
+    }
+
     public static void printStock(ArrayList<ArrayList<String>> booksOnStock){
         // Lista de nombres de generos (actualizable)
         ArrayList<String>genderNick = new ArrayList<>();
@@ -21,12 +28,33 @@ public class PFBiblioteca{
             System.out.println(); 
         }
     }
+    public static void imprimirTodoLibros(ArrayList<ArrayList<String>> booksOnStock){
+        for(int i = 0; i < booksOnStock.size(); i++)
+            System.out.println(booksOnStock.get(i));
+    }    
+    public static void buscarLibro(ArrayList<ArrayList<String>> booksOnStock){
+        for(int i = 0; i < booksOnStock.size(); i++){
+            for(int j = 0; j < booksOnStock.get(i).size(); j++){
+                System.out.println(booksOnStock.get(i).get(j));
 
+
+
+
+
+
+            }
+            
+        }
+    }
+
+    
     public static void main(String[] args){
 
         //Variables
-        String newUserRegist;               // registo de nuevos usuarios | variable acumulada en una lista
-        String newBookRegist;                // registro de un nuevo ingreso de un libro 
+        //String newUserRegist;               // registo de nuevos usuarios | variable acumulada en una lista
+        //String newBookRegist;                // registro de un nuevo ingreso de un libro 
+        Scanner scanner = new Scanner(System.in);
+        
         // Listas de géneros 
         ArrayList<String> Terror = new ArrayList<>();
         ArrayList<String> Ficcion = new ArrayList<>();
@@ -38,9 +66,8 @@ public class PFBiblioteca{
 
         //  Listas 
 
-        //  Lista de generos
-            // lista de almacenamiento de libros en existencia | sub listas en base a géneros/autores
-        ArrayList<ArrayList<String>> booksOnStock = new ArrayList<>(); 
+            //  Lista de generos
+        ArrayList<ArrayList<String>> booksOnStock = new ArrayList<>(); // lista de almacenamiento de libros en existencia | sub listas en base a géneros/autores
             booksOnStock.add(Terror);
             Terror.add("It");
             Terror.add("Dracula");
@@ -48,25 +75,58 @@ public class PFBiblioteca{
             Terror.add("The Shinning");
             Terror.add("The Haunting of Hill House");
             booksOnStock.add(Ficcion);
+            Ficcion.add("Alicia en el pais de las maravillas");
             booksOnStock.add(Romance);
+            Romance.add("Titanic");
             booksOnStock.add(Comedia);
+            Comedia.add("La mama de la mama");
             booksOnStock.add(Documentales);
+            Documentales.add("Los perros en la jungla");
             booksOnStock.add(Infantiles);
+            Infantiles.add("Doki");
 
-        ArrayList<String> Users = new ArrayList<>();
+        //ArrayList<String> Users = new ArrayList<>();
         // Interfaz de usuario
         /*
         *  Añadir funciones si hacen falta, de forma progresiva
         */
-        System.out.println("Bienvenido a la biblioteca virtual en Java");
-        System.out.println("¿Qué tarea desea realizar?");
-        System.out.println();
-        System.out.println("\n 1.- Buscar libro. \n 2.- Solicitar libros (max. 2).\n 3.- Registrar nuevo usuario.\n 4.- Registrar/devolver libro.\n 5.-  ");
+        printMenu(scanner);
+        int userChoise = scanner.nextInt();
+
+        switch (userChoise) {
+            case 1:
+                    //  Buscar libro
+                    System.out.println("Registro de todos los libros (prueba):");
+                    imprimirTodoLibros(booksOnStock);
+                    System.out.println("Busque el nombre del libro por el título:");
+                    buscarLibro(booksOnStock);
+
+                break;
+            case 2:
+                    //  Prestar libros
+                    System.out.println("Aguanta");
+                break;
+            case 3: 
+                    // Registrar nuevo usuario
+                    System.out.println("Aguanta");
+                break;
+            case 4: 
+                    //  Registrar nuevo libro / devolver 
+                    System.out.println("Aguanta");
+                break;
+        
+            default:
+                System.out.println("Opción no válida, intentelo de nuevo.");
+                printMenu(scanner);
+        }
     
-        printStock(booksOnStock);
 
-        Libro libro1 = new Libro("It", "Stephen King", "Terror", 5);
 
-        libro1.mostrarInfo();
+
+
+
+//        printStock(booksOnStock);
+//        Libro libro1 = new Libro("It", "Stephen King", "Terror", 5);
+//        libro1.mostrarInfo();
     }
 }
